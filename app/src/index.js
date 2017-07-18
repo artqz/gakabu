8a85df1c9fe605f70de4b93c08b9afdc0de35a11
artqz <djoctuk@yandex.ru>
2017-07-18 20:50:45 +0500

    update

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import App from './components/App.vue'
 import VueRouter from 'vue-router'
 import Users from './pages/Users.vue'
 import Posts from './pages/Posts.vue'
+import PostCreate from './pages/Posts/Create.vue'
 import Home from './pages/Home.vue'
 
 Vue.use(VueRouter)
@@ -10,11 +11,13 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', component: Home },
   { path: '/users', component: Users },
-  { path: '/posts', component: Posts }
+  { path: '/posts', component: Posts },
+  { path: '/posts/create', component: PostCreate }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 });
 
 new Vue({
---
+++ app/src/pages/Posts/Create.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="post-create">
+    <div class="post-title">
+      Заголовок
+    </div>
+    <div class="post-game">
+      Выбор игры
+    </div>
+    <div class="post-body">
+      <ul class="fields-list">
+        <li class="field" v-for="(field, index) in editor.bodyFields">
+          <div class="field-input" v-if="field.type == 'text'">
+            <medium-editor :text='field.value' :options="options" custom-tag='div' v-on:edit='processEditOperation' />
+          </div>
+          <div v-if="field.type == 'image'">
+            <img :src="field.value" alt="">
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="post-tags">
+      Тэги
+    </div>
+    <div class="post-settings">
+      Настройки {{text}}
+    </div>
+  </div>
+</template>
+
+<script>
+  import editor from 'vue2-medium-editor'
+
+  export default {
+    components: {
+      'medium-editor': editor
+    },
+    data () {
+      return {
+        editor: {
+          title: '',
+          gameId: '',
+          gameTitle: '',
+          bodyFields: [
+            {type: 'text', value: '1'}
+          ]
+        },
+        options: {
+          toolbar: {
+            buttons: ['bold', 'strikethrough', 'h1']
+          }
+        },
+        text: ''
+      }
+    },
+    methods: {
+      processEditOperation (operation) {
+        console.log(operation);
+        // this.editor.bodyFields[0].value = event.event.target.innerText
+        this.editor.bodyFields[0].value = operation.event.srcElement.innerHTML
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
