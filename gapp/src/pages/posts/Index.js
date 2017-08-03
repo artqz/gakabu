afb073041c7eab1be6a6b3b9bccf265ea4720374
artqz <djoctuk@yandex.ru>
2017-08-03 17:02:50 +0500

    posts

--- gapp/src/pages/posts/Index.js
+++ gapp/src/pages/posts/Index.js
@@ -17,6 +17,22 @@ class Index extends Component {
                 <NavLink to='/feed/best'><li className="posts-right-menu-item">Лучшие</li></NavLink>
               </ul>
             </div>
+            <div className="module popular-games page-block">
+              <div className="module-header">
+                Популярные игры
+              </div>
+              <div className="module-body">
+                <div className="module-line clear-fix">
+                  <div className="module-image">
+                    <img src="http://localhost:8000/images/games/icons/1.jpg" />
+                  </div>
+                  <div className="module-info">
+                    <div className="name">Xyu Game 5</div>
+                    <div className="genre">Тактическая хуйня</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="wide-column">
